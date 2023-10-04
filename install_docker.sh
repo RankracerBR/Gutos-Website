83bf414e47cb9305902c5959a03adc81f3deb598
Augusto Pontes <101043200+RankracerBR@users.noreply.github.com>
2023-10-04 01:57:25 -0300

    Delete install_docker.sh

--- install_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo ">>Instalando o Docker"
-
-sudo snap install docker         # version 20.10.17, or
-sudo apt  install docker.io      # version 20.10.12-0ubuntu4
-sudo apt  install podman-docker  # version 3.4.4+ds1-1ubuntu1
-
-echo ">>Instalação Finalizada"
