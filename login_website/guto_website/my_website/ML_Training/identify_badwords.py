26217914e5ca2280be1130702e2d40dacd8f5e3e
Augusto Pontes <augustopontes010@gmail.com>
2024-01-06 21:01:55 -0300

    minor changes

--- login_website/guto_website/my_website/ML_Training/identify_badwords.py
+++ login_website/guto_website/my_website/ML_Training/identify_badwords.py
@@ -1,5 +1,6 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from django.conf import settings
 import pandas as pd
 import smtplib
 
@@ -19,39 +20,39 @@ def Identify_marked_phrases(text):
 def Send_email_warn(destinatario, assunto, mensagem):
     smtp_server = "smtp.gmail.com"
     smtp_port = 587
-    remetente = "rankracerbr21@gmail.com"
-    senha = "pexqzhwcosqimbbf" 
+    remetente = settings.EMAIL_HOST_USER
+    senha = settings.EMAIL_HOST_PASSWORD
 
-    # Conecta ao servidor SMTP
+
     server = smtplib.SMTP(smtp_server, smtp_port)
     server.starttls()
     server.login(remetente, senha)
 
-    # Cria a mensagem
+
     msg = MIMEMultipart()
     msg["From"] = remetente
     msg["To"] = destinatario
     msg["Subject"] = assunto
     msg.attach(MIMEText(mensagem, "plain"))
 
-    # Envia a mensagem
+
     server.sendmail(remetente, destinatario, msg.as_string())
     server.quit()
 
-# Carrega os dados dos usuários
+
 df = pd.read_csv('ML_Training/Users_csv/dados_usuarios.csv')
 
-# Verifica palavras ofensivas nos campos 'username' e 'description'
+
 df['marked_words'] = df['username'].apply(Identify_marked_phrases)
 df['marked_names'] = df['description'].apply(Identify_marked_phrases)
 
-# Analisa se ultrapassa o limite e envia e-mail de aviso
+
 for index, row in df.iterrows():
     if row['marked_words'] >= limite_palavras_ofensivas or row['marked_names'] >= limite_palavras_ofensivas:
         destinatario = row['email']
         assunto = "Aviso: Conteúdo ofensivo detectado"
         mensagem = f"Prezado usuário,\n\nDetectamos palavras ofensivas em seu texto. Por favor, reveja e edite o conteúdo.\n\nAtenciosamente,\nEquipe do Guto."
-        Send_email_warn(destinatario, assunto, mensagem)
+        Send_email_warn(destinatario, assunto, mensagem) 
 
-# Salva o DataFrame atualizado
+
 df.to_csv('ML_Training/Users_csv/dados_usuarios_com_palavras_alvo.csv', index=False)
